bc2ecaa68e2c81e378d62a5ef487bc09c8232764
Shalini Patel <shalllini0504@gmail.com>
2025-09-23 01:16:40 +0530

    add frontend files

---
+++ editor-frontend/src/data/Folders.js
@@ -0,0 +1,29 @@
+const folders = {
+    id: 1,
+    title: 'Data Structures & Algorithms',
+    files: [
+        { 
+            id: 1, 
+            name: 'Binary Heap Implementation', 
+            language: 'java',
+            code: 'System.out.println("Hello, World!");',
+            lastModified: '1 week ago' 
+        },
+        { 
+            id: 2, 
+            name: 'Graph Traversal Algorithms', 
+            language: 'cpp', 
+            code: 'cout << "Hello, World!" << endl;',
+            lastModified: '1 day ago' 
+        },
+        { 
+            id: 3, 
+            name: 'Dynamic Programming Solutions', 
+            language: 'python',
+            code: 'print("Hello, World!")', 
+            lastModified: '3 days ago' 
+        }
+    ]
+};
+
+export default folders;
